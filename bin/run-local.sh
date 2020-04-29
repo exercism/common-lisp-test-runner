e343851932d06cbd447d023433550e280023182b
Brooks J Rady <b.j.rady@gmail.com>
2020-04-29 13:17:47 +0100

    Add required flags to docker run

--- bin/run-local.sh
+++ bin/run-local.sh
@@ -7,6 +7,8 @@ local_output=`realpath $3`
 image=`docker build -q .`
 
 docker run \
+       --network none \
+       --read-only \
        --mount type=bind,source=${local_input},target=/input \
        --mount type=bind,source=${local_output},target=/output \
        --rm \
