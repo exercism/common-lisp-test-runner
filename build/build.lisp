e1c20176fd735e03d5be7693a0c2b4d7bda43758
Mark Simpson <verdammelt@gmail.com>
2021-06-15 21:58:03 -0400

    Update to use clfoundation sbcl image.
    This change also includes a switch from ADD to RUN curl to get
    quicklisp. ADD was giving a 'invalid not-modifed Etag' error which
    kept the image from building.

--- build/build.lisp
+++ build/build.lisp
@@ -1,6 +1,8 @@
-(load "quicklisp/setup")
+(load "quicklisp/setup.lisp")
 (ql:quickload "test-runner")
 
-(sb-ext:save-lisp-and-die "test-runner"
+(let ((bin-dir (make-pathname :directory '(:relative "bin"))))
+  (ensure-directories-exist bin-dir)
+  (sb-ext:save-lisp-and-die (merge-pathnames "test-runner" bin-dir)
                           :toplevel #'test-runner:test-runner
-                          :executable t)
+                          :executable t))
